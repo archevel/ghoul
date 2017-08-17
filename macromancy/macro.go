d2e7a8d0f4413e4a93b1c11f95509a519fa7f8ec
Emil Hellman <emil.hellman@gmail.com>
2017-08-17 14:28:56 +0200

    Add ability to expand macro body with bindings

--- macromancy/macro.go
+++ macromancy/macro.go
@@ -4,17 +4,46 @@ import (
 	e "github.com/archevel/ghoul/expressions"
 )
 
+type bindings map[e.Identifier]e.Expr
+
 type Macro struct {
 	Pattern e.Expr
+	Body    e.Expr
 }
 
-func (m Macro) Matches(expr e.Expr) (bool, map[e.Identifier]e.Expr) {
+func (m Macro) Matches(expr e.Expr) (bool, bindings) {
 	toMatch, macroArgs := identifierAndArgs(m.Pattern)
+
 	candidate, codeArgs := identifierAndArgs(expr)
 	if candidate.Equiv(toMatch) && argsSeemOk(macroArgs, codeArgs) {
 		return true, zipArgs(macroArgs, codeArgs)
 	}
 	return false, nil
+}
+
+func (m Macro) Expand(bound bindings) (e.Expr, error) {
+	toWalk := m.Body
+	res := walkAndReplace(toWalk, bound)
+	return res, nil
+}
+
+func walkAndReplace(toWalk e.Expr, bound bindings) e.Expr {
+
+	if id, ok := toWalk.(e.Identifier); ok {
+		replacement, present := bound[id]
+
+		if present {
+			return replacement
+		}
+
+	}
+
+	if list, ok := toWalk.(e.List); ok && list != e.NIL {
+		h := list.Head()
+		return &e.Pair{walkAndReplace(h, bound), walkAndReplace(list.Tail(), bound)}
+	}
+
+	return toWalk
 }
 
 func identifierAndArgs(expr e.Expr) (e.Expr, []e.Expr) {
@@ -23,7 +52,9 @@ func identifierAndArgs(expr e.Expr) (e.Expr, []e.Expr) {
 	if list, ok := expr.(e.List); ok {
 		identifier = list.Head()
 		argExprs = []e.Expr{}
-		for ; ok && list.Tail() != e.NIL; list, ok = list.Tail().(e.List) {
+		list = list.Tail().(e.List)
+
+		for ; ok && list != e.NIL; list, ok = list.Tail().(e.List) {
 			argExprs = append(argExprs, list.Head())
 		}
 	}
@@ -31,8 +62,8 @@ func identifierAndArgs(expr e.Expr) (e.Expr, []e.Expr) {
 	return identifier, argExprs
 }
 
-func zipArgs(macroArgs []e.Expr, codeArgs []e.Expr) map[e.Identifier]e.Expr {
-	bindings := map[e.Identifier]e.Expr{}
+func zipArgs(macroArgs []e.Expr, codeArgs []e.Expr) bindings {
+	bindings := bindings{}
 	for i, mArg := range macroArgs {
 		identifier := mArg.(e.Identifier)
 		bindings[identifier] = codeArgs[i]
--- macromancy/macro_test.go
+++ macromancy/macro_test.go
@@ -1,6 +1,7 @@
 package macromancy
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -20,7 +21,7 @@ func TestMacrosCanMatchAnExpression(t *testing.T) {
 
 	for _, c := range cases {
 
-		macro := Macro{c.pattern}
+		macro := Macro{c.pattern, nil}
 
 		parseOk, parseRes := parser.Parse(strings.NewReader(c.in))
 
@@ -39,15 +40,15 @@ func TestMacrosBindCorrectly(t *testing.T) {
 	cases := []struct {
 		in               string
 		pattern          e.Expr
-		expectedBindings map[e.Identifier]e.Expr
+		expectedBindings bindings
 	}{
 		{"foo", e.Identifier("foo"), nil},
 		{"(bar)", e.Pair{e.Identifier("bar"), e.NIL}, nil},
 
-		{"(baz 1)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.NIL}}, map[e.Identifier]e.Expr{
+		{"(baz 1)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.NIL}}, bindings{
 			e.Identifier("x"): e.Integer(1),
 		}},
-		{"(baz 1 `foo`)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.Pair{e.Identifier("y"), e.NIL}}}, map[e.Identifier]e.Expr{
+		{"(baz 1 `foo`)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.Pair{e.Identifier("y"), e.NIL}}}, bindings{
 			e.Identifier("x"): e.Integer(1),
 			e.Identifier("y"): e.String("foo"),
 		}},
@@ -55,19 +56,33 @@ func TestMacrosBindCorrectly(t *testing.T) {
 
 	for _, c := range cases {
 
-		macro := Macro{c.pattern}
+		macro := Macro{c.pattern, nil}
 
 		parseOk, parseRes := parser.Parse(strings.NewReader(c.in))
 
 		if parseOk != 0 {
 			t.Fatal("Parsing code failed")
 		}
-
-		if _, bindings := macro.Matches(parseRes.Expressions.Head()); len(bindings) != len(c.expectedBindings) {
+		_, bindings := macro.Matches(parseRes.Expressions.Head())
+		if len(bindings) != len(c.expectedBindings) {
 			t.Errorf(`Macro %s did not bind corretly for %s. Expected %d bindings got %d`,
 				c.pattern.Repr(), c.in, len(c.expectedBindings), len(bindings))
 		}
 
+		for k, expectedValue := range c.expectedBindings {
+			value := bindings[k]
+			if value == nil {
+				t.Errorf("Expected value %s for key %s got nil!", expectedValue.Repr(), k)
+			} else if !expectedValue.Equiv(value) {
+				t.Errorf("Expected value %s for key %s in bindings, got %s", expectedValue.Repr(), k.Repr(), value.Repr())
+			}
+		}
+
+		for k, value := range bindings {
+			if !value.Equiv(c.expectedBindings[k]) {
+				t.Errorf("Found value %s for key %s in macro bindings that is not present in the expected bindings", value, k)
+			}
+		}
 	}
 }
 
@@ -84,7 +99,7 @@ func TestMacrosDoesNotMatchNonMatchingPatterns(t *testing.T) {
 
 	for _, c := range cases {
 
-		macro := Macro{c.pattern}
+		macro := Macro{c.pattern, nil}
 
 		parseOk, parseRes := parser.Parse(strings.NewReader(c.in))
 
@@ -97,6 +112,55 @@ func TestMacrosDoesNotMatchNonMatchingPatterns(t *testing.T) {
 		}
 
 	}
+}
+
+func TestMacroExpansion(t *testing.T) {
+	cases := []struct {
+		expectedRepr string
+		body         e.Expr
+		bound        bindings
+	}{
+		{"foo", e.Identifier("foo"), nil},
+		{"(bar)", e.Pair{e.Identifier("bar"), e.NIL}, nil},
+
+		{"(baz 1)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.NIL}}, bindings{
+			e.Identifier("x"): e.Integer(1),
+		}},
+		{"(baz 1 \"foo\")", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.Pair{e.Identifier("y"), e.NIL}}}, bindings{
+			e.Identifier("x"): e.Integer(1),
+			e.Identifier("y"): e.String("foo"),
+		}},
+	}
+
+	for _, c := range cases {
+		macro := Macro{nil, c.body}
+		expanded, err := macro.Expand(c.bound)
+
+		if err != nil {
+			t.Errorf("Got error '%s' when expanding macro body %s with bindings %s", err, c.body, c.bound)
+		}
+
+		if expanded.Repr() != c.expectedRepr {
+			t.Errorf("Expected %s after expanding macro, but got %s", c.expectedRepr, expanded.Repr())
+		}
+	}
+}
+
+func ExampleSwapMacro() {
+
+	// (swap x y) - (let ((tmp x)) (set! x y) (set! y tmp))
+	_, pattern := parser.Parse(strings.NewReader("(swap x y)"))
+	_, body := parser.Parse(strings.NewReader("(let ((tmp x)) (set! x y) (set! y tmp))"))
+	_, code := parser.Parse(strings.NewReader("(swap foo bar)"))
+
+	//	fmt.Println("\npat:", pattern.Expressions.Repr(), "\nbody:", body.Expressions.Repr(), "\ncode:", code.Expressions.Repr())
+	macro := Macro{pattern.Expressions.(e.List).Head(), body.Expressions.(e.List).Head()}
+	_, bound := macro.Matches(code.Expressions.(e.List).Head())
+
+	res, _ := macro.Expand(bound)
+	fmt.Println(res.Repr())
+	// Output:
+	// (let ((tmp foo)) (set! foo bar) (set! bar tmp))
 }
 
 type DefineMacro struct{}
