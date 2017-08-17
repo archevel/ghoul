a59c706b516b2a40625aa992b9e0292bfa549bfc
Emil Hellman <emil.hellman@gmail.com>
2017-08-17 08:36:46 +0200

    Make macro matches return a binding map
    The map keys are the macro identifoers that should be replaced in the
    macro body.

--- macromancy/macro.go
+++ macromancy/macro.go
@@ -8,23 +8,38 @@ type Macro struct {
 	Pattern e.Expr
 }
 
-func (m Macro) Matches(expr e.Expr) bool {
-	toMatch, macroArgCount := identifierAndArgCount(m.Pattern)
-	candidate, codeArgCount := identifierAndArgCount(expr)
-	return candidate.Equiv(toMatch) && macroArgCount == codeArgCount
+func (m Macro) Matches(expr e.Expr) (bool, map[e.Identifier]e.Expr) {
+	toMatch, macroArgs := identifierAndArgs(m.Pattern)
+	candidate, codeArgs := identifierAndArgs(expr)
+	if candidate.Equiv(toMatch) && argsSeemOk(macroArgs, codeArgs) {
+		return true, zipArgs(macroArgs, codeArgs)
+	}
+	return false, nil
 }
 
-func identifierAndArgCount(expr e.Expr) (e.Expr, int) {
+func identifierAndArgs(expr e.Expr) (e.Expr, []e.Expr) {
 	identifier := expr
-	argCount := -1
-
+	var argExprs []e.Expr = nil
 	if list, ok := expr.(e.List); ok {
 		identifier = list.Head()
-		argCount = 0
+		argExprs = []e.Expr{}
 		for ; ok && list.Tail() != e.NIL; list, ok = list.Tail().(e.List) {
-			argCount++
+			argExprs = append(argExprs, list.Head())
 		}
 	}
 
-	return identifier, argCount
+	return identifier, argExprs
+}
+
+func zipArgs(macroArgs []e.Expr, codeArgs []e.Expr) map[e.Identifier]e.Expr {
+	bindings := map[e.Identifier]e.Expr{}
+	for i, mArg := range macroArgs {
+		identifier := mArg.(e.Identifier)
+		bindings[identifier] = codeArgs[i]
+	}
+	return bindings
+}
+
+func argsSeemOk(macroArgs []e.Expr, codeArgs []e.Expr) bool {
+	return (macroArgs == nil && codeArgs == nil) || (macroArgs != nil && codeArgs != nil && len(macroArgs) == len(codeArgs))
 }
--- macromancy/macro_test.go
+++ macromancy/macro_test.go
@@ -22,14 +22,50 @@ func TestMacrosCanMatchAnExpression(t *testing.T) {
 
 		macro := Macro{c.pattern}
 
-		ok, parseRes := parser.Parse(strings.NewReader(c.in))
+		parseOk, parseRes := parser.Parse(strings.NewReader(c.in))
 
-		if ok != 0 {
+		if parseOk != 0 {
 			t.Fatal("Parsing code failed")
 		}
 
-		if !macro.Matches(parseRes.Expressions.Head()) {
+		if ok, _ := macro.Matches(parseRes.Expressions.Head()); !ok {
 			t.Errorf(`Macro %s did not match %s`, c.pattern.Repr(), c.in)
+		}
+
+	}
+}
+
+func TestMacrosBindCorrectly(t *testing.T) {
+	cases := []struct {
+		in               string
+		pattern          e.Expr
+		expectedBindings map[e.Identifier]e.Expr
+	}{
+		{"foo", e.Identifier("foo"), nil},
+		{"(bar)", e.Pair{e.Identifier("bar"), e.NIL}, nil},
+
+		{"(baz 1)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.NIL}}, map[e.Identifier]e.Expr{
+			e.Identifier("x"): e.Integer(1),
+		}},
+		{"(baz 1 `foo`)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.Pair{e.Identifier("y"), e.NIL}}}, map[e.Identifier]e.Expr{
+			e.Identifier("x"): e.Integer(1),
+			e.Identifier("y"): e.String("foo"),
+		}},
+	}
+
+	for _, c := range cases {
+
+		macro := Macro{c.pattern}
+
+		parseOk, parseRes := parser.Parse(strings.NewReader(c.in))
+
+		if parseOk != 0 {
+			t.Fatal("Parsing code failed")
+		}
+
+		if _, bindings := macro.Matches(parseRes.Expressions.Head()); len(bindings) != len(c.expectedBindings) {
+			t.Errorf(`Macro %s did not bind corretly for %s. Expected %d bindings got %d`,
+				c.pattern.Repr(), c.in, len(c.expectedBindings), len(bindings))
 		}
 
 	}
@@ -43,11 +79,52 @@ func TestMacrosDoesNotMatchNonMatchingPatterns(t *testing.T) {
 		{"(foo)", e.Identifier("foo")},
 		{"bar", e.Pair{e.Identifier("bar"), e.NIL}},
 		{"(baz 1 x)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.NIL}}},
+		{"(baz)", e.Pair{e.Identifier("baz"), e.Pair{e.Identifier("x"), e.NIL}}},
 	}
 
 	for _, c := range cases {
 
 		macro := Macro{c.pattern}
+
+		parseOk, parseRes := parser.Parse(strings.NewReader(c.in))
+
+		if parseOk != 0 {
+			t.Fatal("Parsing code failed")
+		}
+
+		if ok, _ := macro.Matches(parseRes.Expressions.Head()); ok {
+			t.Errorf(`Macro %s matched code "%s" which it shouldn't`, c.pattern.Repr(), c.in)
+		}
+
+	}
+}
+
+type DefineMacro struct{}
+
+func (m DefineMacro) Transform(expr e.Expr) (e.Expr, error) {
+	list := expr.(e.List)
+	define := list.Head()
+	list = list.Tail().(e.List)
+	idAndArgs := list.Head().(e.List)
+	id := idAndArgs.Head()
+	args := idAndArgs.Tail()
+	body := list.Tail()
+
+	return e.Pair{define, e.Pair{id, e.Pair{e.Pair{e.Identifier("lambda"), e.Pair{args, body}}, e.NIL}}}, nil
+}
+
+func TestMacroTransform(t *testing.T) {
+	cases := []struct {
+		in  string
+		out string
+	}{
+		{
+			`(define (foo x) x)`, `(define foo (lambda (x) x))`,
+		},
+	}
+	for _, c := range cases {
+
+		macro := DefineMacro{}
 
 		ok, parseRes := parser.Parse(strings.NewReader(c.in))
 
@@ -55,9 +132,15 @@ func TestMacrosDoesNotMatchNonMatchingPatterns(t *testing.T) {
 			t.Fatal("Parsing code failed")
 		}
 
-		if macro.Matches(parseRes.Expressions.Head()) {
-			t.Errorf(`Macro %s matched code "%s" which it shouldn't`, c.pattern.Repr(), c.in)
+		res, err := macro.Transform(parseRes.Expressions.Head())
+
+		if err != nil {
+			t.Errorf("Could not transform %s into %s\n", c.in, c.out)
 		}
 
+		if res.Repr() != c.out {
+			t.Errorf("Transform of %s did not give expected result %s, instead got %+v", c.in, c.out, res.Repr())
+		}
 	}
+
 }
