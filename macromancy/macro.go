9b32cd7daf97faed4491450bc3da67314a619f69
Emil Hellman <emil.hellman@gmail.com>
2017-08-29 09:39:01 +0200

    Refactor matchWalk splitting it into more funcs

--- macromancy/macro.go
+++ macromancy/macro.go
@@ -31,14 +31,11 @@ func (m Macro) Expand(bound bindings) (e.Expr, error) {
 }
 
 func walkAndReplace(toWalk e.Expr, bound bindings) e.Expr {
-
 	if id, ok := toWalk.(e.Identifier); ok {
 		replacement, present := bound[id]
-
 		if present {
 			return replacement
 		}
-
 	}
 
 	if list, ok := toWalk.(e.List); ok && list != e.NIL {
@@ -51,61 +48,70 @@ func walkAndReplace(toWalk e.Expr, bound bindings) e.Expr {
 
 func matchWalk(macro e.Expr, code e.Expr, bound bindings, hasElipsis bool) (bool, bindings) {
 	if id, ok := macro.(e.Identifier); ok {
-		b, present := bound[id]
-		if present && !b.Equiv(code) {
-			return false, bound
-		}
-		bound[id] = code
-		return true, bound
+		return matchAndBindIdentifier(id, code, bound)
 	}
 	if macroList, macroOk := macro.(e.List); macroOk {
-		codeList, codeOk := code.(e.List)
-		if codeOk {
-
-			if macroList == e.NIL && codeList == e.NIL {
-				return true, bound
-			}
-			macroLength, _ := listLength(macroList)
-			if macroList == e.NIL || (codeList == e.NIL && (!hasElipsis || macroLength > 1)) {
-				return false, nil
-			}
-
-			macHead := macroList.Head()
-			if id, ok := macHead.(e.Identifier); ok && id.Equiv(e.Identifier("...")) {
-				if macroList.Tail() == e.NIL {
-					// bind ... to all of code (1 2 3)
-					return matchWalk(macHead, codeList, bound, true)
-				}
-
-				// there is atleast one part of the pattern following ...
-				followingPatternCount := macroLength - 1
-				bindToMacHead, rest := splitListAt(followingPatternCount, codeList)
-				_, bound := matchWalk(macHead, bindToMacHead, bound, true)
-
-				return matchWalk(macroList.Tail(), rest, bound, true)
-			} else {
-				// head is not an elipsis
-				headMatch, bound := matchWalk(macHead, codeList.Head(), bound, hasElipsis)
-				if headMatch {
-					return matchWalk(macroList.Tail(), codeList.Tail(), bound, hasElipsis)
-				}
-			}
+		if codeList, codeOk := code.(e.List); codeOk {
+			return matchHeadAndTail(macroList, codeList, bound, hasElipsis)
 		} else {
-			// code is not a list so probably single value
-			if macroList != e.NIL && macroList.Tail() == e.NIL {
-				// there is a macro head and it is the last one
-				return matchWalk(macroList.Head(), code, bound, hasElipsis)
-			}
-			if macroList.Head().Equiv(e.Identifier("...")) {
-				// the macro head is ... and there are more macro patterns`
-				bound[macroList.Head().(e.Identifier)] = e.NIL
-				return matchWalk(macroList.Tail(), code, bound, hasElipsis)
-			}
-
+			return matchFinalCodeExpression(macroList, code, bound, hasElipsis)
 		}
 	}
 	if code != nil && macro != nil && code.Equiv(macro) {
 		return true, bound
+	}
+	return false, nil
+}
+
+func matchAndBindIdentifier(id e.Identifier, code e.Expr, bound bindings) (bool, bindings) {
+	b, present := bound[id]
+	if present && !b.Equiv(code) {
+		return false, bound
+	}
+	bound[id] = code
+	return true, bound
+}
+
+func matchHeadAndTail(macroList e.List, codeList e.List, bound bindings, hasElipsis bool) (bool, bindings) {
+	if macroList == e.NIL && codeList == e.NIL {
+		return true, bound
+	}
+	macroLength, _ := listLength(macroList)
+	if macroList == e.NIL || (codeList == e.NIL && (!hasElipsis || macroLength > 1)) {
+		return false, nil
+	}
+
+	if id, ok := macroList.Head().(e.Identifier); ok && id.Equiv(e.Identifier("...")) {
+		return matchElipsis(macroList, macroLength, codeList, bound)
+	} else {
+		headMatch, bound := matchWalk(macroList.Head(), codeList.Head(), bound, hasElipsis)
+		if headMatch {
+			return matchWalk(macroList.Tail(), codeList.Tail(), bound, hasElipsis)
+		}
+	}
+	return false, nil
+}
+
+func matchElipsis(macroList e.List, macroLength int, codeList e.List, bound bindings) (bool, bindings) {
+	macHead := macroList.Head()
+	if macroList.Tail() == e.NIL {
+		return matchWalk(macHead, codeList, bound, true)
+	}
+
+	followingPatternCount := macroLength - 1
+	bindToMacHead, rest := splitListAt(followingPatternCount, codeList)
+	_, bound = matchWalk(macHead, bindToMacHead, bound, true)
+
+	return matchWalk(macroList.Tail(), rest, bound, true)
+}
+
+func matchFinalCodeExpression(macroList e.List, code e.Expr, bound bindings, hasElipsis bool) (bool, bindings) {
+	if macroList != e.NIL && macroList.Tail() == e.NIL {
+		return matchWalk(macroList.Head(), code, bound, hasElipsis)
+	}
+	if macroList.Head().Equiv(e.Identifier("...")) {
+		bound[macroList.Head().(e.Identifier)] = e.NIL
+		return matchWalk(macroList.Tail(), code, bound, hasElipsis)
 	}
 	return false, nil
 }
@@ -120,33 +126,33 @@ func idAndRest(expr e.Expr) (e.Identifier, e.Expr) {
 	return identifier.(e.Identifier), nil
 }
 
-func splitListAt(count int, codeList e.List) (e.Expr, e.Expr) {
+func splitListAt(endCount int, codeList e.List) (e.Expr, e.Expr) {
 
-	firstN := codeList
-	splitPoint := firstN
-	var rest e.Expr
+	begining := codeList
+	splitPoint := begining
+	var ending e.Expr
 	ok := false
-	codeLength, _ := listLength(splitPoint)
-	target := codeLength - count - 1
+	codeLength, _ := listLength(codeList)
+	splitIndex := codeLength - (endCount + 1)
 
-	if target < 0 {
+	if splitIndex < 0 {
 		return e.NIL, codeList
 	}
 
-	for i := 0; i < target; i++ {
+	for i := 0; i < splitIndex; i++ {
 		if sp, ok := splitPoint.Tail().(e.List); ok {
 			splitPoint = sp
 		} else {
 			break
 		}
 	}
-	rest, ok = splitPoint.Tail().(e.List)
-	if splitPoint != e.NIL && (ok || count == 1) {
-		rest = splitPoint.Tail()
+	ending, ok = splitPoint.Tail().(e.List)
+	if splitPoint != e.NIL && (ok || endCount == 1) {
+		ending = splitPoint.Tail()
 		splitPoint.(*e.Pair).T = e.NIL
 	}
 
-	return firstN, rest
+	return begining, ending
 }
 
 func listLength(list e.List) (int, bool) {
