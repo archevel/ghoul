d6f022b1155638d034ae6e45a03f18bc3c266f9b
Emil Hellman <emil.hellman@gmail.com>
2017-09-01 10:11:08 +0200

    Add initial macromancer that can transform code
    Code transformations are read and performed as the code is being walked
    by the macromancer.

--- macromancy/macromancy.go
+++ macromancy/macromancy.go
@@ -5,10 +5,90 @@ import (
 )
 
 type Transformer interface {
-	Transform(list e.List) (e.List, error)
+	Transform(list e.List) (e.Expr, error)
 }
-type Macromancer struct{}
+type Macromancer struct {
+	macroGroups []*MacroGroup
+}
 
-func (m Macromancer) Transform(list e.List) (e.List, error) {
-	return list, nil
+func NewMacromancer() *Macromancer {
+
+	return &Macromancer{}
+}
+
+func (m Macromancer) Transform(inList e.List) (e.Expr, error) {
+	return m.transform(inList)
+}
+
+func (m *Macromancer) transform(expr e.Expr) (e.Expr, error) {
+	if l, ok := expr.(e.List); ok && l != e.NIL {
+		h := l.Head()
+		if sl, ok := subList(l); ok {
+			newH, err := m.expandMacrosAgainst(sl)
+			if err != nil {
+				return nil, err
+			}
+			h = newH
+			if sl, ok := h.(e.List); ok && e.Identifier("define-syntax").Equiv(sl.Head()) {
+				mg, err := NewMacroGroup(sl)
+				if err == nil {
+					m.macroGroups = append(m.macroGroups, mg)
+				}
+
+				if t, ok := tail(l); ok {
+					newT, err := m.expandMacrosAgainst(t)
+					if err != nil {
+						return nil, err
+					}
+					return m.transform(newT)
+				} else {
+					return m.transform(sl.Tail())
+				}
+			}
+
+		}
+
+		h, err := m.transform(h)
+		if err != nil {
+			return nil, err
+		}
+		t, err := m.transform(l.Tail())
+		if err != nil {
+			return nil, err
+		}
+		return &e.Pair{h, t}, nil
+
+	} else {
+		return expr, nil
+	}
+}
+
+func (m *Macromancer) expandMacrosAgainst(subList e.List) (e.Expr, error) {
+	var subExpr e.Expr = subList
+	for _, mg := range m.macroGroups {
+		macros := mg.Matches(subList)
+		if macros != nil {
+			for _, macro := range macros {
+				if ok, bound := macro.Matches(subList); ok {
+					newSubExpr, err := macro.Expand(bound)
+					if err != nil {
+						return nil, err
+					}
+					return newSubExpr, nil
+				}
+			}
+			break
+		}
+	}
+	return subExpr, nil
+}
+
+func tail(l e.List) (e.List, bool) {
+	t, ok := l.Tail().(e.List)
+	return t, ok
+}
+
+func subList(l e.List) (e.List, bool) {
+	h, ok := l.Head().(e.List)
+	return h, ok
 }
--- macromancy/macromancy_test.go
+++ macromancy/macromancy_test.go
@@ -8,32 +8,94 @@ import (
 )
 
 func TestMacromancerDoesNotModifyNonMacroCode(t *testing.T) {
+	code := `(a b c)`
+	expected := `(a b c)`
 
-	cases := []struct {
-		in  string
-		out string
-	}{
-		{`(a b c)`, `((a b c))`},
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerRemovesValidMacrosDefinitionsItEncounters(t *testing.T) {
+	code := `(define-syntax foo (syntax-rules () (((foo x) (x biz)))))`
+	expected := ``
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerAppliesFoundMacrosDefinitions(t *testing.T) {
+	code := `(define-syntax foo (syntax-rules () (((foo x) (x biz)))))  (foo bar)`
+	expected := `(bar biz)`
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerDoesNotAlterCodeBeforeEncounteredMacros(t *testing.T) {
+	code := `(foo bar) (define-syntax foo (syntax-rules () (((foo x) (x biz)))))`
+	expected := `(foo bar)`
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerOnlyReadsMacrosOnAllLevels(t *testing.T) {
+	code := `(baz (define-syntax foo (syntax-rules () (((foo x) (x biz)))))) (foo bar)`
+	expected := `(baz) (bar biz)`
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerOnlyExpandsListsStartingWithMatchIds(t *testing.T) {
+	code := `(define-syntax foo (syntax-rules () (((foo x) (x biz))))) (fiz foo bar)`
+	expected := `(fiz foo bar)`
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerCanExpandMacrosInSubLists(t *testing.T) {
+	code := `(define-syntax foo (syntax-rules () (((foo x) (x biz))))) ((fiz buz) (foo  bar))`
+	expected := `((fiz buz) (bar biz))`
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerCanHandlePairs(t *testing.T) {
+	code := `(define-syntax foo (syntax-rules () (((foo x) (x biz))))) ((fiz . buz) (foo  bar))`
+	expected := `((fiz . buz) (bar biz))`
+
+	runMacroTest(t, code, expected)
+}
+
+func TestMacromancerCanExpandToNewMacro(t *testing.T) {
+	code := `
+(define-syntax foo
+  (syntax-rules ()
+	  (
+			((foo x) ((define-syntax x (syntax-rules () (((x) (biz))))) x)))))
+(foo bar) (bar)
+`
+	expected := `(biz) (biz)`
+
+	runMacroTest(t, code, expected)
+}
+
+func runMacroTest(t *testing.T, code string, expected string) {
+	ok, parsedCode := parser.Parse(strings.NewReader(code))
+	if ok != 0 {
+		t.Errorf("Failed to parse code: %s\n", code)
 	}
 
-	for _, c := range cases {
-		ok, parseRes := parser.Parse(strings.NewReader(c.in))
+	var transformer Transformer = NewMacromancer()
 
-		if ok != 0 {
-			t.Errorf("Failed to parse: %s\n", c)
-		}
-
-		var transformer Transformer = Macromancer{}
-		mancedCode, err := transformer.Transform(parseRes.Expressions)
-
-		if err != nil {
-			t.Errorf(`Got error: "%s" when mancing code: %s`, err, c)
-		}
-
-		if mancedCode.Repr() != c.out {
-			t.Errorf(`Macromancing "%s" failed, should have the result "%s", but the result was "%s"`, c.in, c.out, mancedCode.Repr())
-		}
-
+	ok, parsedExpected := parser.Parse(strings.NewReader(expected))
+	if ok != 0 {
+		t.Errorf("Failed to parse expected: %s\n", expected)
 	}
 
+	mancedCode, err := transformer.Transform(parsedCode.Expressions)
+
+	if err != nil {
+		t.Errorf(`Got error: "%s" when mancing code %s`, err, code)
+	}
+
+	if err == nil && !mancedCode.Equiv(parsedExpected.Expressions) {
+		t.Errorf("Expected code:\n'%s'\n\n to yield:\n '%s'\n\n after macromancy transform, but got: \n'%s'", parsedCode.Expressions.Repr(), parsedExpected.Expressions.Repr(), mancedCode.Repr())
+	}
 }
