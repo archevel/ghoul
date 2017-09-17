7a4af8ef594b4f2afaec8de188292de5c80a46a5
Emil Hellman <emil.hellman@gmail.com>
2017-09-17 19:18:20 +0200

    Initial commit of wight
    wight is intended to generate wrappers for go code.

---
+++ cmd/wight/wight.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"text/template"
+)
+
+func GenerateFor(path string, input io.Reader, output io.Writer) {
+	io.WriteString(output, "package "+path+"_ghoul\n")
+}
+
+const testSuffix = "_test.go"
+
+func filter(fs os.FileInfo) bool {
+	name := fs.Name()
+	index := len(name) - len(testSuffix)
+	if index < 0 {
+		index = 0
+	}
+	return name[index:] != testSuffix
+
+}
+
+type PreludeValues struct {
+	PkgName string
+	Pkg     string
+	Imports []string
+}
+
+var PreludeTemplate, preludeErr = template.New("PreludeTemplate").Parse(
+	"package {{.PkgName}}_ghoul\n\n" +
+		"import (\n{{range .Imports}}\t\"{{.}}\"\n{{end}}" +
+		"\n\t\"{{.Pkg}}\"" +
+		"\n\tev \"github.com/archevel/ghoul/evaluator\"" +
+		"\n\te \"github.com/archevel/ghoul/expressions\"\n)\n")
+
+func CreatePrelude(pkg string, imports []string, w io.Writer) error {
+	if preludeErr != nil {
+		return preludeErr
+	}
+	pkgName := pkg
+	splitOn := strings.LastIndex(pkg, "/") + 1
+	if splitOn >= 0 {
+		pkgName = pkg[splitOn:]
+	}
+
+	return PreludeTemplate.Execute(w, PreludeValues{pkgName, pkg, imports})
+}
+
+const FuncTemplate = `
+func {{.FuncName}}(args e.List, ev *ev.Evaluator) (e.Expr, error) {
+	{{.GeneratedBody}}
+}
+`
+
+var BuiltInTypeTemplate, bittErr = template.New("BuiltInTypeTemplate").Parse(`
+	var arg{{.N}} {{.Type}}
+
+	switch v := args.Head().(type) {
+		case e.{{.BuiltInType}}:
+			arg{{.N}} = {{.Type}}(v)
+		case *e.{{.BuiltInType}}:
+			arg{{.N}} = {{.Type}}(*v)
+		case e.Foreign:
+			switch vv := v.Val().(type) {
+			case *{{.Type}}:
+				arg{{.N}} = *vv
+			case {{.Type}}:
+				arg{{.N}} = vv
+			default:
+				return nil, errors.New("Could not convert arg{{.N}} to {{.Type}}")
+			}
+		case *e.Foreign:
+			switch vv := v.Val().(type) {
+			case *{{.Type}}:
+				arg{{.N}} = *vv
+			case {{.Type}}:
+				arg{{.N}} = vv
+			default:
+				return nil, errors.New("Could not convert arg{{.N}} to {{.Type}}")
+			}
+		default:
+			return nil, errors.New("Could not convert arg{{.N}} to {{.Type}}")
+	}
+`)
+
+var StructAndInterfaceTemplate, saitErr = template.New("StructAndInterfaceTemplate").Parse(`
+	var arg{{.N}} {{.Type}}
+
+	switch f := args.Head().(type) {
+		case e.Foreign: fallthrough
+			switch v := f.Val().(type) {
+			case *{{.Type}}:
+				arg{{.N}} = *v
+			case {{.Type}}:
+				arg{{.N}} = v
+			default:
+				return nil, errors.New("Could not converrt arg{{.N}} to {{.Type}}")
+			}
+	case *e.Foreign: 
+			switch v := f.Val().(type) {
+			case *{{.Type}}:
+				arg{{.N}} = *v
+			case {{.Type}}:
+				arg{{.N}} = v
+			default:
+				return nil, errors.New("Could not converrt arg{{.N}} to {{.Type}}")
+			}
+		default:
+			return nil, errors.New("Could not converrt arg{{.N}} to {{.Type}}")				
+	}
+`)
+
+type ArgTemplateValues struct {
+	N           int
+	Type        string
+	BuiltInType string
+}
+
+func ConvertArg(n int, wantedType string, builtInType string, w io.Writer) error {
+	if bittErr != nil {
+		return bittErr
+	}
+	if saitErr != nil {
+		return saitErr
+	}
+	vals := ArgTemplateValues{n, wantedType, builtInType}
+	if builtInType != "" {
+		return BuiltInTypeTemplate.Execute(w, vals)
+	} else {
+		return StructAndInterfaceTemplate.Execute(w, vals)
+	}
+}
+
+func main() {
+	fmt.Println("")
+}
---
+++ cmd/wight/wight_test.go
@@ -0,0 +1,290 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestGeneratesPackageName(t *testing.T) {
+
+	cases := []struct {
+		path                string
+		expectedPackageLine string
+	}{
+		{"com/lo", "package com/lo_ghoul"},
+		{"github.com/bar", "package github.com/bar_ghoul"},
+		{"github.com/foo/bar", "package github.com/foo/bar_ghoul"},
+	}
+
+	for _, c := range cases {
+
+		r := strings.NewReader("")
+		buffer := new(bytes.Buffer)
+		GenerateFor(c.path, r, buffer)
+		res := buffer.String()
+
+		if res[:len(c.expectedPackageLine)] != c.expectedPackageLine {
+			t.Errorf("Expected first line to be %s. Got %s", c.expectedPackageLine, res)
+		}
+	}
+
+}
+
+func TestPrelude(t *testing.T) {
+	expected :=
+		"package pkg_ghoul\n\n" +
+			"import (\n\t\"fmt\"\n\t\"os\"\n" +
+			"\n\t\"path/to/pkg\"" +
+			"\n\tev \"github.com/archevel/ghoul/evaluator\"" +
+			"\n\te \"github.com/archevel/ghoul/expressions\"\n)\n"
+
+	buffer := new(bytes.Buffer)
+	w := bufio.NewWriter(buffer)
+	err := CreatePrelude("path/to/pkg", []string{"fmt", "os"}, w)
+
+	w.Flush()
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	actual := buffer.String()
+	if actual != expected {
+		t.Errorf("Failed to create prelude. Expected :\n%s\n, got: \n%s", expected, actual)
+	}
+
+}
+
+func TestBooleanTemplate(t *testing.T) {
+
+	var expected = `
+	var arg9 bool
+
+	switch v := args.Head().(type) {
+		case e.Boolean:
+			arg9 = bool(v)
+		case *e.Boolean:
+			arg9 = bool(*v)
+		case e.Foreign:
+			switch vv := v.Val().(type) {
+			case *bool:
+				arg9 = *vv
+			case bool:
+				arg9 = vv
+			default:
+				return nil, errors.New("Could not convert arg9 to bool")
+			}
+		case *e.Foreign:
+			switch vv := v.Val().(type) {
+			case *bool:
+				arg9 = *vv
+			case bool:
+				arg9 = vv
+			default:
+				return nil, errors.New("Could not convert arg9 to bool")
+			}
+		default:
+			return nil, errors.New("Could not convert arg9 to bool")
+	}
+`
+
+	buffer := new(bytes.Buffer)
+	w := bufio.NewWriter(buffer)
+	err := ConvertArg(9, "bool", "Boolean", w)
+	w.Flush()
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	actual := buffer.String()
+	if actual != expected {
+		t.Errorf("Converting argumenet failed. Wanted:\n%s\n, got: \n%s", expected, actual)
+	}
+}
+
+func TestStringTemplate(t *testing.T) {
+
+	var expected = `
+	var arg5 string
+
+	switch v := args.Head().(type) {
+		case e.String:
+			arg5 = string(v)
+		case *e.String:
+			arg5 = string(*v)
+		case e.Foreign:
+			switch vv := v.Val().(type) {
+			case *string:
+				arg5 = *vv
+			case string:
+				arg5 = vv
+			default:
+				return nil, errors.New("Could not convert arg5 to string")
+			}
+		case *e.Foreign:
+			switch vv := v.Val().(type) {
+			case *string:
+				arg5 = *vv
+			case string:
+				arg5 = vv
+			default:
+				return nil, errors.New("Could not convert arg5 to string")
+			}
+		default:
+			return nil, errors.New("Could not convert arg5 to string")
+	}
+`
+
+	buffer := new(bytes.Buffer)
+	w := bufio.NewWriter(buffer)
+	err := ConvertArg(5, "string", "String", w)
+	w.Flush()
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	actual := buffer.String()
+	if actual != expected {
+		t.Errorf("Converting argumenet failed. Wanted:\n%s\n, got: \n%s", expected, actual)
+	}
+}
+
+func TestFloatTemplate(t *testing.T) {
+
+	var expected = `
+	var arg1 float
+
+	switch v := args.Head().(type) {
+		case e.Float:
+			arg1 = float(v)
+		case *e.Float:
+			arg1 = float(*v)
+		case e.Foreign:
+			switch vv := v.Val().(type) {
+			case *float:
+				arg1 = *vv
+			case float:
+				arg1 = vv
+			default:
+				return nil, errors.New("Could not convert arg1 to float")
+			}
+		case *e.Foreign:
+			switch vv := v.Val().(type) {
+			case *float:
+				arg1 = *vv
+			case float:
+				arg1 = vv
+			default:
+				return nil, errors.New("Could not convert arg1 to float")
+			}
+		default:
+			return nil, errors.New("Could not convert arg1 to float")
+	}
+`
+
+	buffer := new(bytes.Buffer)
+	w := bufio.NewWriter(buffer)
+	err := ConvertArg(1, "float", "Float", w)
+	w.Flush()
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	actual := buffer.String()
+	if actual != expected {
+		t.Errorf("Converting argumenet failed. Wanted:\n%s\n, got: \n%s", expected, actual)
+	}
+}
+
+func TestIntegerTemplate(t *testing.T) {
+
+	var expected = `
+	var arg1 int
+
+	switch v := args.Head().(type) {
+		case e.Integer:
+			arg1 = int(v)
+		case *e.Integer:
+			arg1 = int(*v)
+		case e.Foreign:
+			switch vv := v.Val().(type) {
+			case *int:
+				arg1 = *vv
+			case int:
+				arg1 = vv
+			default:
+				return nil, errors.New("Could not convert arg1 to int")
+			}
+		case *e.Foreign:
+			switch vv := v.Val().(type) {
+			case *int:
+				arg1 = *vv
+			case int:
+				arg1 = vv
+			default:
+				return nil, errors.New("Could not convert arg1 to int")
+			}
+		default:
+			return nil, errors.New("Could not convert arg1 to int")
+	}
+`
+
+	buffer := new(bytes.Buffer)
+	w := bufio.NewWriter(buffer)
+	err := ConvertArg(1, "int", "Integer", w)
+	w.Flush()
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	actual := buffer.String()
+	if actual != expected {
+		t.Errorf("Converting argumenet failed. Wanted:\n%s\n, got: \n%s", expected, actual)
+	}
+}
+
+func TestStructAndInterfaceTemplate(t *testing.T) {
+	var expected = `
+	var arg99 Ball
+
+	switch f := args.Head().(type) {
+		case e.Foreign: fallthrough
+			switch v := f.Val().(type) {
+			case *Ball:
+				arg99 = *v
+			case Ball:
+				arg99 = v
+			default:
+				return nil, errors.New("Could not converrt arg99 to Ball")
+			}
+	case *e.Foreign: 
+			switch v := f.Val().(type) {
+			case *Ball:
+				arg99 = *v
+			case Ball:
+				arg99 = v
+			default:
+				return nil, errors.New("Could not converrt arg99 to Ball")
+			}
+		default:
+			return nil, errors.New("Could not converrt arg99 to Ball")				
+	}
+`
+	buffer := new(bytes.Buffer)
+	w := bufio.NewWriter(buffer)
+	err := ConvertArg(99, "Ball", "", w)
+	w.Flush()
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	actual := buffer.String()
+	if actual != expected {
+		t.Errorf("Converting argumenet failed. Wanted:\n%s\n, got: \n%s", expected, actual)
+	}
+}
+
+func TestFunctionArgTemplate(t *testing.T) {
+	t.Log("Not implemented...")
+}
